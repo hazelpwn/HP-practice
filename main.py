ef66845d6cf6ac560590c7fe3efc0296b99b0a0c
Anna Carlisle <117605899+hazelpwn@users.noreply.github.com>
2022-11-08 13:41:59 +0000

    initial math game

---
+++ main.py
@@ -0,0 +1,25 @@
+# this is how you get input!
+y = input("hello welcome what is your name:")
+print("Hello", y)
+hp ="hwwiyn"
+userpass = input("what's your password:")
+if userpass ==hp:
+  print("correct pass welcome")
+else:
+  print("wrong password")
+  exit(0)
+print("WELCOME TO THE MATH GAME!!!")
+x =2*8
+print("what is 2x8?")
+y=int(input())
+if x==y:
+  print("correct")
+else:
+  print ("wrong")
+y =1*9
+print("what is 1x9")
+x =int(input)()
+if y==x:
+  print("correct")
+else:
+  print("wrong")
